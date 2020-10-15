5fa5f11b31c0359182bc31fb402afbc2e59d2a0d
Serjo <Serjo@DESKTOP-TRAVELM>
2020-10-16 01:18:11 +0300

    ыва

---
+++ Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите два числа");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                int rep = 0;
+
+                if (a > b)
+                {
+                    int buf = a;
+                    a = b;
+                    b = buf;
+                }
+
+                while (a <= b)
+                {
+                    if (a % 7 == 0)
+                    {
+                        rep += a;
+                        //Console.Write(a + " ");               чтобы вывести промежуточные значения
+                    }
+                    a++;
+                }
+
+                //Console.WriteLine();                          чтобы ответ был с новой строки
+                Console.WriteLine(rep);
+            }
+        }
+    }
+}
