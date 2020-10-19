009df5b7cfb1052fa39e322e3f0c4831a6a91041
Serjo <Serjo@DESKTOP-TRAVELM>
2020-10-19 21:12:31 +0300

    Добавлен вариант решения 8й задачи

---
+++ Cubic_Root(8(2))/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Cubic_Root_8_2__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите целое число, кубический корень которого хотите вычислить: ");
+                double n;
+                n = Convert.ToInt32(Console.ReadLine());
+                double rightBoarder = n;
+                double leftBoarder = 0;
+                double midPoint = 0;
+               
+                while (rightBoarder - leftBoarder >= 0.000001)
+                {
+                    midPoint = (leftBoarder + rightBoarder) / 2;
+
+                    if (midPoint * midPoint * midPoint == n)
+                        break;
+                    if (midPoint * midPoint * midPoint > n)
+                    {
+                        rightBoarder = midPoint;
+                    }
+                    else
+                    {
+                        leftBoarder = midPoint;
+                    }
+                }
+                Console.WriteLine(midPoint);
+            }
+        }
+
+    }
+}
+    
+
